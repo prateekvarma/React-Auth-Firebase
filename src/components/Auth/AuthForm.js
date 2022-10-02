34cfc505c90e6889f20bf0a7564f477d05c049ab
Prateek Varma <prtk.varma@gmail.com>
2022-10-02 17:28:54 -0600

    Added Firebase user creation, and console logged errors if any

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,25 +1,58 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 import classes from './AuthForm.module.css';
 
 const AuthForm = () => {
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+
   const [isLogin, setIsLogin] = useState(true);
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
   };
 
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+
+    if(isLogin) {
+
+    } else {
+      fetch("https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_KEY]", {
+        method: "POST",
+        body: JSON.stringify({ 
+          email: enteredEmail, 
+          password: enteredPassword, 
+          returnSecureToken: true 
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        }
+      }).then((res) => {
+        if(res.ok) {
+
+        } else {
+          //error in response
+          return res.json().then((data) => console.log(data));
+        }
+      });
+    }
+  };
+
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <input ref={emailInputRef} type='email' id='email' required />
         </div>
         <div className={classes.control}>
           <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
+          <input ref={passwordInputRef} type='password' id='password' required />
         </div>
         <div className={classes.actions}>
           <button>{isLogin ? 'Login' : 'Create Account'}</button>
