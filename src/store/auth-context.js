9e696fc2658058b8fd6afcac6ce227171ebf8613
Prateek Varma <prtk.varma@gmail.com>
2022-10-06 14:51:49 -0600

    Auto login/logout logic based on firebase's token timeout

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
+
+let logoutTimer;
 
 const AuthContext = React.createContext({
   token: "",
@@ -15,24 +17,61 @@ const calculateTokenExpiration = (expirationTime) => {
   return remainingTime;
 }
 
+const retrieveStoredToken = () => {
+    const storedToken = localStorage.getItem('token');
+    const storedExpirationTime = localStorage.getItem('expirationTime');
+
+    const remainingTime = calculateTokenExpiration(storedExpirationTime);
+
+    if (remainingTime <= 60000) {
+      //if remainingTime is less than 1 minute, remove login token
+      localStorage.removeItem('token');
+      localStorage.removeItem('expirationTime');
+      return null;
+    }
+
+    return {
+      token: storedToken,
+      duration: remainingTime
+    };
+};
+
 export const AuthContextProvider = (props) => {
-  const initialToken = localStorage.getItem('token');//it's either set, or its undefined
+  const tokenData = retrieveStoredToken();
+  let initialToken;
+  if (tokenData) {
+    initialToken = tokenData.token;
+  } //otherwise initialToken stays undefined
+
   const [token, setToken] = useState(initialToken);
 
   const isUserLoggedIn = !!token; //returns truthy is there is a token
 
-  const logoutHandler = () => {
+  const logoutHandler = useCallback (() => {
     setToken(null);
     localStorage.removeItem('token');
-  };
+
+    if (logoutTimer) {
+      clearTimeout(logoutTimer);
+    }
+  }, []);
 
   const loginHandler = (token, expirationTime) => {
     setToken(token);
     localStorage.setItem('token', token);
+    localStorage.setItem('expirationTime', expirationTime);
 
     const remainingTime = calculateTokenExpiration(expirationTime);
-    setTimeout(logoutHandler, remainingTime);
+    logoutTimer = setTimeout(logoutHandler, remainingTime);
   };
+
+  useEffect(() => {
+    //if stored token is there, set a timeout for the remaining time - this is called for auto login
+    if(tokenData) {
+      console.log(tokenData.duration)
+      logoutTimer = setTimeout(logoutHandler, tokenData.duration);
+    }
+  }, [tokenData, logoutHandler]);
 
   const contextValue = {
     token: token,
